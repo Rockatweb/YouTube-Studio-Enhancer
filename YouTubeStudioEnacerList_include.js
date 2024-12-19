9981bd5679b6a6a162662152d6b9736ebca4866b
dannycurth <danny.curth@diva-e.com>
2024-12-19 17:09:14 +0100

    script for list view

---
+++ YouTubeStudioEnacerList_include.js
@@ -0,0 +1,44 @@
+// ==UserScript==
+// @name         YouTube Studio Enhancer
+// @namespace    youtube
+// @version      0.01
+// @description  A tooling to make YouTube Studio much better
+// @author       You
+// @match        https://studio.youtube.com/channel/UCCP6P-4slm9zxgtT6NoJ2ag/videos/upload*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @require      https://raw.githubusercontent.com/Rockatweb/YouTube-Studio-Enhancer/refs/heads/main/YouTubeStudioEnhancerList.js?token=GHSAT0AAAAAAC4FBWQ5AMM2PYNFGXPC4UZSZ3EGQZQ
+// @grant        GM_xmlhttpRequest
+// @run-at       document-start
+// ==/UserScript==
+
+(function () {
+  'use strict';
+
+  function addScriptToHead(scriptContent) {
+    const headElement = document.querySelector('head');
+    if (headElement) {
+      const scriptElement = document.createElement('script');
+      scriptElement.innerHTML = scriptContent;
+      headElement.appendChild(scriptElement);
+    } else {
+      const observer = new MutationObserver((mutations) => {
+        if (document.head) {
+          observer.disconnect();
+          const scriptElement = document.createElement('script');
+          scriptElement.innerHTML = scriptContent;
+          document.head.appendChild(scriptElement);
+        }
+      });
+      observer.observe(document.documentElement, { childList: true, subtree: true });
+    }
+  }
+
+  GM_xmlhttpRequest({
+    method: 'GET',
+    url: 'https://raw.githubusercontent.com/Rockatweb/YouTube-Studio-Enhancer/refs/heads/main/YouTubeStudioEnhancerList.js?token=GHSAT0AAAAAAC4FBWQ5AMM2PYNFGXPC4UZSZ3EGQZQ',
+    onload: (ev) => {
+      addScriptToHead(ev.responseText);
+    },
+  });
+
+})();
---
+++ YouTubeStudioEnhancerDetails.js
@@ -0,0 +1,76 @@
+window.addEventListener('load', function() {
+  function onclickButton() {
+    document.querySelector('main').scrollTo(0, document.querySelector('ytcp-video-details-section').scrollHeight);
+
+
+    console.log('toggle meee');
+    const toggleButton = document.querySelector('ytcp-button#toggle-button button');
+
+    toggleButton.click();
+
+
+    setTimeout(() => {
+      const alteredData = document.querySelector('ytkp-altered-content-select .altered-content-options .altered-content-option-row:nth-child(2) tp-yt-paper-radio-button');
+      console.log(alteredData);
+      if (alteredData) {
+        alteredData.click();
+      } else {
+        alert('altered Data radio not found');
+      }
+
+
+      const autoLocation = document.querySelector('ytcp-video-metadata-editor-advanced .ytcp-video-metadata-editor-advanced:nth-child(8) ytcp-checkbox-lit');
+      if (autoLocation) {
+        autoLocation.click();
+      } else {
+        alert('location checkbox not found');
+      }
+
+      const autoConcept = document.querySelector('ytcp-video-metadata-editor-advanced .ytcp-video-metadata-editor-advanced:nth-child(11) ytcp-form-checkbox');
+      if (autoConcept) {
+        autoConcept.click();
+      } else {
+        alert('auto concept checkbox not found');
+      }
+
+      const catContainer = document.querySelector('ytcp-video-metadata-editor-advanced .ytcp-video-metadata-editor-advanced#category-container');
+      if (catContainer) {
+        catContainer.scrollIntoView();
+      }
+    }, 4000);
+  }
+
+  function observeCheckbox () {
+    const button = document.createElement('button');
+    button.innerText = 'Toggle';
+    document.querySelector('ytcp-entity-page-header h1').appendChild(button);
+
+    button.addEventListener('click', ()=>{
+      onclickButton();
+      return false;
+    });
+  }
+
+  function waitForAddedNode(params, callback) {
+    const el = document.querySelector(params.elm);
+
+    new MutationObserver((mutations, observer) => {
+      if (el) {
+        observer.disconnect();
+        callback();
+      }
+    }).observe(el, {
+      attributes: true,
+      childList: true,
+      subtree: true,
+      characterData: true
+    });
+  }
+
+  waitForAddedNode({
+    elm: 'ytcp-video-details-section',
+    recursive: true,
+  }, el => {
+    observeCheckbox();
+  });
+});
